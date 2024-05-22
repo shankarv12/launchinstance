f668dcb455b9cc791194b3b8e64135b956a47c36
shankarv12 <shankarvenkat1998@gmail.com>
2024-05-22 08:34:02 +0530

    first commit

---
+++ launch_instance.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+aws ec2 run-instances \
+  --image-id ami-016485166ec7fa705 \
+  --instance-type t2.micro \
+  --key-name testpair \
+  --security-group-ids sg-0f6e24df4b5984210 \
+  --subnet-id subnet-02e26c30fc87b14ea\
+  --region us-east-1
